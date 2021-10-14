6578779329c1c52ed5af7e97d31bf79eb372cb3f
Prof Jean Paul <jean.paul@uniritter.edu.br>
2021-10-14 17:58:37 -0300

    Criação das entidades criação de mockup de Service e Repository criação do controller com /get

---
+++ Controllers/CourseController.cs
@@ -0,0 +1,31 @@
+﻿using Cafeine_DinDin_Backend.Entities;
+using Cafeine_DinDin_Backend.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cafeine_DinDin_Backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CourseController : ControllerBase
+    {
+        
+        private readonly ILogger<CourseController> _logger;
+
+        public CourseController(ILogger<CourseController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Course>> Get()
+        {
+         
+            return CourseService.GetInstance().GetCourses();
+        }
+    }
+}
---
+++ Services/CourseService.cs
@@ -0,0 +1,33 @@
+﻿using Cafeine_DinDin_Backend.Entities;
+using Cafeine_DinDin_Backend.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cafeine_DinDin_Backend.Services
+{
+    public class CourseService
+    {
+        private static CourseService instance;
+
+        private CourseService()
+        {
+
+        }
+        public static CourseService GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new CourseService();
+            }
+            return instance;
+        }
+        public List<Course> GetCourses()
+        {
+
+            CourseRepository _repo = CourseRepository.GetInstance();
+            return _repo.findAll();
+        }
+    }
+}
